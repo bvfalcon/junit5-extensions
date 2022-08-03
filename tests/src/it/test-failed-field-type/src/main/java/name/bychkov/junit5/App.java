ef4be10e1d2fc236844578a6637f20d3ee47fe7a
Vladimir V. Bychkov <github@bychkov.name>
2022-08-03 17:59:00 +0200

    Compilation errors in integration tests solved

--- tests/src/it/test-failed-field-type/src/main/java/name/bychkov/junit5/App.java
+++ tests/src/it/test-failed-field-type/src/main/java/name/bychkov/junit5/App.java
@@ -2,7 +2,7 @@ package name.bychkov.junit5;
 
 public class App
 {
-	@CheckField(targetClass = SimpleDTO.class, value = "CONSTANT_1" type = double.class)
+	@CheckField(targetClass = SimpleDTO.class, value = "CONSTANT_1", type = double.class)
 	private String field;
 	
 	public static void main(String[] args)
--- tests/src/it/test-failed-method-returntype/src/main/java/name/bychkov/junit5/App.java
+++ tests/src/it/test-failed-method-returntype/src/main/java/name/bychkov/junit5/App.java
@@ -2,7 +2,7 @@ package name.bychkov.junit5;
 
 public class App
 {
-	@CheckMethod(targetClass = SimpleDTO.class, value = "staticMethod2" returnType = double.class)
+	@CheckMethod(targetClass = SimpleDTO.class, value = "staticMethod2", returnType = double.class)
 	public static void main(String[] args)
 	{
 	}
